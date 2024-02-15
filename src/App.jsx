660e98b347fdd9e15502b6d24dde6643872c6f7b
lekhireddyvineeth <lekhireddyvineeth@gmail.com>
2024-02-15 19:14:53 +0000

    1 st setupt

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import ProductsScreen from './Components/ProductsScreen'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <ProductsScreen/>
+      </div>
+    </>
+  )
+}
+
+export default App
